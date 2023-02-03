4669aa9104cd1aabf3b6a6f502daa903f1a4b3eb
Vicent-PM <114580711+Vicent-PM@users.noreply.github.com>
2023-02-03 14:28:37 +0100

    subido

--- Programacion/2-Trimestre/Proyectos Android/EjDosVistas/app/src/main/java/com/example/ejdosvistas/MainActivity.kt
+++ Programacion/2-Trimestre/Proyectos Android/EjDosVistas/app/src/main/java/com/example/ejdosvistas/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.ejdosvistas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import com.example.ejdosvistas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,9 +13,31 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
+        binding.seekBar.progress = 500
 
+        binding.seekBar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                var progreso = 100 + binding.seekBar.progress
+                binding.textView3.setText(progreso.toString() + "€")
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+        })
+
+        binding.button.setOnClickListener {
+            binding.user.setText("")
+            binding.password.setText("")
+            binding.seekBar.progress = 500
+        }
 
     }
 }
